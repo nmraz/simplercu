7d398d038be3d287ed59ee9e6bc291e8922e9c55
Noam Raz <noamraz8@gmail.com>
2025-03-03 21:16:03 +0200

    Add build option for store-buffering test
    It looks like some of the store buffering cases the asymmetric barriers
    are supposed to prevent never actually happen in practice because the
    thread-local `read_lock_nesting` and `need_qs` variables reside in the
    same cache line.
    
    By forcibly placing them in separate cache lines, we can actually cause
    crashes when the asymmetric barriers are missing, which is pretty cool.

--- src/rcu_internal.h
+++ src/rcu_internal.h
@@ -7,13 +7,22 @@
 #define CACHE_LINE_SIZE 64
 #define CACHE_ALIGNED alignas(CACHE_LINE_SIZE)
 
+#ifdef TEST_STORE_BUFFERING
+// When we're testing store buffering prevention, make sure `need_qs` resides in
+// a separate cache line from `read_lock_nesting`.
+#define NEW_CACHELINE_FOR_STORE_BUFFERING_TEST CACHE_ALIGNED
+#else
+#define NEW_CACHELINE_FOR_STORE_BUFFERING_TEST
+#endif
+
 #define unlikely(X) __builtin_expect(!!(X), 0)
 
 struct rcu_thread_state {
     atomic_int read_lock_nesting;
-    atomic_bool need_qs;
     struct rcu_thread_state* next;
     struct rcu_thread_state** pprev;
+
+    NEW_CACHELINE_FOR_STORE_BUFFERING_TEST atomic_bool need_qs;
 };
 
 extern thread_local CACHE_ALIGNED struct rcu_thread_state rcu_thread_state;
